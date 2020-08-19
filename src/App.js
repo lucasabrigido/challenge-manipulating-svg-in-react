d4f2f9b4726b8e5dbb0ddc1037365e2b28a27842
Lucas Brígido <lucasbrigido@fortalsistemas.com.br>
2020-08-19 15:46:13 -0300

    Manipulando svg, nomeando partes do corpo - responsivo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Avatar from './containers/avatar/avatar';
+
+import './assets/scss/stylesGlobais.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Avatar/>
+    </div>
+  );
+}
+
+export default App;
